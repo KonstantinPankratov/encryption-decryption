042cf982f993ba86487a59ae77b37956059c4ea8
Pankratov Konstantin <pankratovkm@gmail.com>
2018-01-14 15:56:23 +0500

    Improved and redone using OOP.

--- index.php
+++ index.php
@@ -1,38 +1,71 @@
 <?php
 
-	$key = "Secret!"; // define secret key for encryption and decryption
+class encryption_decryption {
 
-	function encryption($string){
+	private $key = "Secret!"; // define secret key for encryption and decryption
 
-		global $key;
+	function processing( $string, $encrypt ){
+
 		$in = $string;
 		$out = ""; // define variable for encrypt
 
 		for ($i = 0; $i < strlen($in); $i++)
 		{
-				$out .= $in[$i] ^ $key[$i % strlen($key)];
+			$encrypt ? $out .= ( $in[$i] ^ $this->key[ $i % strlen($this->key) ] ) << 2 
+					 : $out .= ( $in[$i] ^ $this->key[ $i % strlen($this->key) ] );
 		}
 		
 		return $out;
 	}
 
-	function decryption($string){
+}
 
-		global $key;
-		$in = $string;
-		$out = ""; // define variable for encrypt
+$ED = new encryption_decryption();
 
-		for ($i = 0; $i < strlen($in); $i++)
-		{
-				$out .= $in[$i] ^ $key[$i % strlen($key)];
-		}
-		
-		return $out;
-	}
+$input_text  = "Hello World!";
 
-	$cipher = encryption("Hello World!");
-	echo "Cipher: " . $cipher . "<br>";
+$cipher      = $ED->processing($input_text, "true");
 
-	$source_text = decryption($cipher);
-	echo "Source text: " . $source_text . "<br>";
+$source_text = $ED->processing($cipher, "false");
+
 ?>
+
+<!-- tables for visual displaying -->
+
+<table border="1" cellpadding="0" cellspacing="0">
+	<tr>
+		<th>Input text</th>
+	</tr>
+	<tr>
+		<td><?php echo $input_text; ?></td>
+	</tr>
+</table>
+
+<span>></span>
+
+<table border="1" cellpadding="0" cellspacing="0">
+	<tr>
+		<th>Cipher</th>
+	</tr>
+	<tr>
+		<td><?php echo $cipher; ?></td>
+	</tr>
+</table>
+
+<span>></span>
+
+<table border="1" cellpadding="0" cellspacing="0">
+	<tr>
+		<th>Source code</th>
+	</tr>
+	<tr>
+		<td><?php echo $source_text; ?></td>
+	</tr>
+</table>
+
+<!-- styles for table to make it looks a bit more convenient -->
+
+<style>
+ table{text-align: left; display:inline-block; vertical-align: middle;}
+ table tr td, table tr th {padding: 6px 15px;}
+</style>
